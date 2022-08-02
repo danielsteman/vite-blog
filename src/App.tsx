20b7dea074e65ea85dfd2ee2f7b4d2c4a6f3d9c8
danielsteman <daniel-steman@live.nl>
2022-08-02 23:28:30 +0200

    :recycle: refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,17 @@ import React from 'react';
 import './App.css';
 import {
   Heading,
-  Grid,
-  GridItem,
 } from '@chakra-ui/react';
-import {
-  MdInfo, MdAutoGraph, MdConstruction, MdContactPage,
-} from 'react-icons/md';
 import PageBase from './components/PageBase';
 import BlogPost from './components/BlogPost';
-import SideMenuButton from './components/SideMenuButton';
 
 const App = () => (
   <PageBase>
-    <Grid templateColumns="repeat(4, 1fr)">
-      <GridItem colSpan={1} py={8}>
-        <SideMenuButton icon={MdInfo} text="About" url="" />
-        <SideMenuButton icon={MdAutoGraph} text="Experience" url="" />
-        <SideMenuButton icon={MdConstruction} text="Work" url="" />
-        <SideMenuButton icon={MdContactPage} text="Contact" url="" />
-      </GridItem>
-      <GridItem colSpan={3} p={16}>
-        <Heading fontSize="36px" fontWeight={750} py={4}>
-          Blog
-        </Heading>
-        <BlogPost title="First blog post" date="Mon 1 Aug, 2022" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, corrupti praesentium nihil asperiores earum harum nesciunt, reprehenderit labore eos quaerat optio quam tempora saepe ea. Nobis et reprehenderit voluptate vitae!" />
-        <BlogPost title="Second blog post" date="Mon 2 Aug, 2022" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, corrupti praesentium nihil asperiores earum harum nesciunt, reprehenderit labore eos quaerat optio quam tempora saepe ea. Nobis et reprehenderit voluptate vitae!" />
-      </GridItem>
-    </Grid>
+    <Heading fontSize="36px" fontWeight={750} py={4}>
+      Blog
+    </Heading>
+    <BlogPost title="First blog post" date="Mon 1 Aug, 2022" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, corrupti praesentium nihil asperiores earum harum nesciunt, reprehenderit labore eos quaerat optio quam tempora saepe ea. Nobis et reprehenderit voluptate vitae!" />
+    <BlogPost title="Second blog post" date="Mon 2 Aug, 2022" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, corrupti praesentium nihil asperiores earum harum nesciunt, reprehenderit labore eos quaerat optio quam tempora saepe ea. Nobis et reprehenderit voluptate vitae!" />
   </PageBase>
 );
 
--- src/components/PageBase.tsx
+++ src/components/PageBase.tsx
@@ -5,12 +5,18 @@ import {
   Spacer,
   Center,
   ChakraProvider,
+  Grid,
+  GridItem,
 } from '@chakra-ui/react';
 import { FaGithub, FaStackOverflow, FaLinkedin } from 'react-icons/fa';
+import {
+  MdInfo, MdAutoGraph, MdConstruction, MdContactPage,
+} from 'react-icons/md';
 import ColorModeSwitcher from './ColorModeSwitcher';
 import Logo from './Logo';
 import customTheme from '../themes/theme';
 import NavigationButton from './NavigationButton';
+import SideMenuButton from './SideMenuButton';
 
 interface Props {
   children: React.ReactNode;
@@ -32,8 +38,18 @@ const PageBase: React.FC<Props> = ({ children }) => (
           <ColorModeSwitcher mr={4} />
         </Center>
       </Flex>
-      {children}
     </Box>
+    <Grid templateColumns="repeat(4, 1fr)" textAlign="left">
+      <GridItem colSpan={1} py={8}>
+        <SideMenuButton icon={MdInfo} text="About" url="" />
+        <SideMenuButton icon={MdAutoGraph} text="Experience" url="" />
+        <SideMenuButton icon={MdConstruction} text="Work" url="" />
+        <SideMenuButton icon={MdContactPage} text="Contact" url="" />
+      </GridItem>
+      <GridItem colSpan={3} p={16}>
+        {children}
+      </GridItem>
+    </Grid>
   </ChakraProvider>
 );
 
