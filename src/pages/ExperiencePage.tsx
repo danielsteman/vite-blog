0bcb970b0ec28930a4335fa1d692d1ee209d55eb
danielsteman <daniel-steman@live.nl>
2022-08-09 00:00:58 +0200

    :construction: api call wip

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
 import PageTitle from '../components/PageTitle';
 import PageBase from './PageBase';
 
@@ -7,6 +12,17 @@ const ExperiencePage = () => {
   return (
     <PageBase>
       <PageTitle title={title} />
+      <Box py={4}>
+        <Heading size="lg" fontWeight={400}>
+          Data engineer
+        </Heading>
+        <Heading size="md" fontWeight={700}>
+          a.s.r.
+        </Heading>
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore non soluta reiciendis animi odit, iste ipsam dicta sunt error facere voluptatibus, maiores itaque amet esse rem vero aliquid dolore cum!
+        </Text>
+      </Box>
     </PageBase>
   );
 };
