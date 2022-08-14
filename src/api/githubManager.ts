686093e5d09fdeb82089dffde6f26586e186ad15
danielsteman <daniel-steman@live.nl>
2022-08-14 13:39:32 +0200

    :construction: intergrating github api calls

--- src/api/githubManager.ts
+++ src/api/githubManager.ts
@@ -1,12 +1,11 @@
 import { request } from '@octokit/request';
-import 'dotenv/config';
 
 export const getRepoLanguage = async (repo: string) => {
   // example: await getRepoLanguage(repoName);
   const response = await request('GET /repos/danielsteman/{repo}/languages', {
     repo,
     headers: {
-      authorization: `token ${process.env.PAT}`,
+      authorization: `token ${import.meta.env.VITE_PAT}`,
     },
   }).then((resp) => resp.data);
   return response;
@@ -16,12 +15,11 @@ export const getRepos = async () => {
   // example: await getRepos;
   const response = await request('GET /users/danielsteman/repos', {
     headers: {
-      authorization: `token ${process.env.PAT}`,
+      authorization: `token ${import.meta.env.VITE_PAT}`,
     },
   }).then((resp) => resp.data.map(
     async (repo: any) => {
       const languages = await getRepoLanguage(repo.name);
-      console.log(languages);
       return (
         {
           name: repo.name,
@@ -31,8 +29,5 @@ export const getRepos = async () => {
       );
     },
   ));
-  return response;
+  return Promise.all(response);
 };
-
-const x = await getRepos();
-console.log(x);
--- src/pages/WorkPage.tsx
+++ src/pages/WorkPage.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import { Box } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { getRepos } from '../api/githubManager';
 import PageTitle from '../components/PageTitle';
 import PageBase from './PageBase';
 
+interface RepoData {
+  [index: number]: { name: string, description: string, languages: [string] }
+}
+
 const WorkPage = () => {
   const title = 'Work';
+
+  const [repos, setRepos] = useState<RepoData | null>([]);
+
+  useEffect(() => {
+    (async function getReposAsync() {
+      const res = await getRepos();
+      setRepos(res);
+    }());
+  }, []);
+
   return (
     <PageBase>
       <PageTitle title={title} />
+      <Box>
+        {repos.map((repo) => (
+          <Box p={4}>
+            {repo.name}
+            {repo.description}
+          </Box>
+        ))}
+      </Box>
     </PageBase>
   );
 };
