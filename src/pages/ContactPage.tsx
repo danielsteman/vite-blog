50d48249252c6c24dbebc62f6cbbd23318898b3c
danielsteman <daniel-steman@live.nl>
2022-08-16 22:02:46 +0200

    :bug: contact page fix

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,49 +1,40 @@
-import { useFormik } from 'formik';
 import {
   FormControl, FormLabel, Input, FormHelperText, Textarea, Button,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import PageTitle from '../components/PageTitle';
 import PageBase from './PageBase';
 
 const ContactPage = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      message: '',
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  (
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    console.log(email);
+    console.log(message);
+    event.preventDefault();
+  };
+
+  return (
     <PageBase>
       <PageTitle title="Contact" />
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          variant="filled"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          isRequired
-        />
-        <FormHelperText>Your email will not be shared.</FormHelperText>
-        <Textarea
-          placeholder="Message"
-          mt={2}
-          id="message"
-          name="message"
-          variant="filled"
-          onChange={formik.handleChange}
-          value={formik.values.message}
-          isRequired
-        />
-      </FormControl>
-      <Button type="submit" mt={2}>Send</Button>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" onChange={handleEmailChange} value={email} isRequired />
+          <FormHelperText>Your email will not be shared.</FormHelperText>
+          <Textarea placeholder="Message" mt={6} onChange={handleMessageChange} value={message} isRequired />
+        </FormControl>
+        <Button type="submit" mt={2}>Send</Button>
+      </form>
     </PageBase>
   );
 };
