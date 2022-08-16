0af092ffc7b3da4cac9f74eb3d4cc3ce2606ac7e
danielsteman <daniel-steman@live.nl>
2022-08-16 22:04:20 +0200

    :speech_balloon: added content

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
     <PageBase>
       <PageTitle title={title} />
       <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloremque exercitationem, corrupti, nulla hic fugiat dolor excepturi, eaque eum neque distinctio earum voluptates labore sunt! Culpa assumenda veritatis laborum minus?
+        Enthousiastic about most tech related things and always learning.
       </Text>
     </PageBase>
   );
