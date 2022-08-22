4df9212f77d34db0e3d97bdcf55c738f38668ec9
danielsteman <daniel-steman@live.nl>
2022-08-22 23:34:53 +0200

    :construction: blog post WIP

--- src/routes/BlogPost.tsx
+++ src/routes/BlogPost.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import remarkInlineLinks from 'remark-inline-links';
 import PageTitle from '../components/PageTitle';
 import Base from './Base';
-import { html } from '../content/blog1.md';
+import { ReactComponent } from '../content/blog1.md';
 
 const BlogPost = () => {
   const params = useParams();
   return (
     <Base>
       <PageTitle title={`title of blog ${params.blogId}`} />
-      <ReactMarkdown remarkPlugins={[remarkGfm, remarkInlineLinks]}>
-        {html}
-      </ReactMarkdown>
+      <ReactComponent />
     </Base>
   );
 };
-
+// eslint-disable-next-line implicit-arrow-linebreak
 export default BlogPost;
