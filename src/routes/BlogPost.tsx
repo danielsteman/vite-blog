02a55441733573201ab4fb96193de07a169ffd15
danielsteman <daniel-steman@live.nl>
2022-08-22 23:12:53 +0200

    :construction: blog post WIP

--- src/routes/BlogPost.tsx
+++ src/routes/BlogPost.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkInlineLinks from 'remark-inline-links';
 import PageTitle from '../components/PageTitle';
 import Base from './Base';
+import { html } from '../content/blog1.md';
 
 const BlogPost = () => {
   const params = useParams();
   return (
     <Base>
       <PageTitle title={`title of blog ${params.blogId}`} />
+      <ReactMarkdown remarkPlugins={[remarkGfm, remarkInlineLinks]}>
+        {html}
+      </ReactMarkdown>
     </Base>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { plugin, Mode } from 'vite-plugin-markdown';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), plugin({ mode: [Mode.HTML, Mode.TOC, Mode.REACT] })],
   resolve: {
     alias: {
       'node-fetch': 'isomorphic-fetch',
