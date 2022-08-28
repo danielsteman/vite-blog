e5bb2d3293b8bdc2cf4eb7981431879a52165557
danielsteman <daniel-steman@live.nl>
2022-08-28 22:29:25 +0200

    :sparkles: finish dynamic markdown fetching

--- src/routes/BlogPost.tsx
+++ src/routes/BlogPost.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { useParams } from 'react-router-dom';
 import PageTitle from '../components/PageTitle';
 import Base from './Base';
 
 const BlogPost = () => {
   const [text, setText] = useState('');
+  const params = useParams();
 
   useEffect(() => {
-    fetch('../content/blog1.md')
+    fetch(`../content/blog${params.blogId}.md`)
       .then((res) => res.text())
       .then((res) => setText(res));
   }, []);
