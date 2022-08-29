633500a2152dbd1007e4ea9d96eeffebb776b19a
danielsteman <daniel-steman@live.nl>
2022-08-29 21:53:35 +0200

    :recycle: refactoring style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import About from './routes/About';
 import Blog from './routes/Blog';
 import BlogPost from './routes/BlogPost';
--- src/routes/Base.tsx
+++ src/routes/Base.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {
-  Box,
   Flex,
   Spacer,
   ChakraProvider,
@@ -9,6 +8,8 @@ import {
   MenuButton,
   IconButton,
   Menu,
+  useMediaQuery,
+  Box,
 } from '@chakra-ui/react';
 import {
   FaGithub, FaStackOverflow, FaLinkedin, FaLastfmSquare,
@@ -26,11 +27,12 @@ interface Props {
 }
 
 const Base: React.FC<Props> = ({ children }) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [isMobile] = useMediaQuery('(max-width: 30em)');
   return (
     <ChakraProvider theme={customTheme}>
-      <Box textAlign="left" fontSize="l">
-        <Flex flexDirection={['column', 'row', 'row', 'row', 'row']}>
+      <Box maxW={1280} textAlign="center" p="2rem" my={0} mx="auto">
+        <Flex textAlign="left" flexDirection="row">
           <Link to="/">
             <Logo
               h="8vmin"
@@ -38,34 +40,40 @@ const Base: React.FC<Props> = ({ children }) => {
             />
           </Link>
           <Spacer />
-          {open
-            ? (
-              <Box>
-                <NavigationButton url="https://www.github.com/danielsteman" icon={FaGithub} />
-                <NavigationButton url="https://www.stackoverflow.com/users/11383969/dsteman" icon={FaStackOverflow} />
-                <NavigationButton url="https://www.linkedin.com/in/danielsteman/" icon={FaLinkedin} />
-                <NavigationButton url="https://www.last.fm/user/daniel-steman" icon={FaLastfmSquare} />
-                <ColorModeSwitcher />
-              </Box>
-            )
-            : (
-              <MenuButton
-                as={IconButton}
-                icon={<HamburgerIcon />}
-                variant="outline"
-                onClick={() => setOpen(!open)}
-              />
-            )}
+          <Flex flexDirection={['column', 'row', 'row', 'row', 'row']}>
+            <Menu>
+              {isMobile
+                ? (
+                  <MenuButton
+                    as={IconButton}
+                    icon={<HamburgerIcon />}
+                    variant="outline"
+                    onClick={() => setOpen(!open)}
+                  />
+                ) : null}
+              {!isMobile || (isMobile && open)
+                ? (
+                  <Menu>
+                    <NavigationButton url="https://www.github.com/danielsteman" icon={FaGithub} />
+                    <NavigationButton url="https://www.stackoverflow.com/users/11383969/dsteman" icon={FaStackOverflow} />
+                    <NavigationButton url="https://www.linkedin.com/in/danielsteman/" icon={FaLinkedin} />
+                    <NavigationButton url="https://www.last.fm/user/daniel-steman" icon={FaLastfmSquare} />
+                    <ColorModeSwitcher />
+                  </Menu>
+                )
+                : null}
+            </Menu>
+          </Flex>
         </Flex>
+        <Grid templateColumns="repeat(4, 1fr)" textAlign="left">
+          <GridItem colSpan={1} py={8}>
+            <SideMenu />
+          </GridItem>
+          <GridItem colSpan={3} py={16} pr={16}>
+            {children}
+          </GridItem>
+        </Grid>
       </Box>
-      <Grid templateColumns="repeat(4, 1fr)" textAlign="left">
-        <GridItem colSpan={1} py={8}>
-          <SideMenu />
-        </GridItem>
-        <GridItem colSpan={3} py={16} pr={16}>
-          {children}
-        </GridItem>
-      </Grid>
     </ChakraProvider>
   );
 };
