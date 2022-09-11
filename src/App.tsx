841f321b030bf59f438416cae246b94e4ca61fee
danielsteman <daniel-steman@live.nl>
2022-09-11 21:42:16 +0200

    :bug: fix(router): Fix routes
    fix(router): Fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ import Work from './routes/Work';
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Blog />} />
     <Route path="blog/:blogId" element={<BlogPost />} />
     <Route path="blog" element={<Blog />} />
     <Route path="about" element={<About />} />
     <Route path="experience" element={<Experience />} />
     <Route path="work" element={<Work />} />
     <Route path="contact" element={<Contact />} />
+    <Route path="/" element={<Blog />} />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
