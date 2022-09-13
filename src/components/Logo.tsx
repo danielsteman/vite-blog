b69bdb26ae9fcd19520c80140cc6d21fe8a09273
danielsteman <daniel-steman@live.nl>
2022-09-13 21:41:00 +0200

    :sparkles: finish logo rotation logic
    feat(logo): Finish logo rotation logic

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,13 +10,14 @@ import logo from '../assets/memoji.png';
 
 const Logo = (props: ImageProps) => {
   const [rotateClockwise, setRotateClockwise] = React.useState(false);
+  const [position, setPosition] = React.useState(0);
 
   const ref = React.useRef<HTMLImageElement>(null);
   const computedStyle = ref.current ? window.getComputedStyle(ref.current) : undefined;
 
   const spin = keyframes`
-  from { transform: rotate(${rotateClockwise ? 0 : 360}deg); }
-  to { transform: rotate(${rotateClockwise ? 360 : 0}deg); }
+  from { transform: rotate(${rotateClockwise ? position : 360}deg); }
+  to { transform: rotate(${rotateClockwise ? 360 : position}deg); }
   `;
   const prefersReducedMotion = usePrefersReducedMotion();
 
@@ -30,7 +31,8 @@ const Logo = (props: ImageProps) => {
         const tf = computedStyle?.transform;
         const sin = tf?.split(', ')[1];
         if (sin) {
-          console.log(Math.round(Math.asin(parseFloat(sin)) * (180 / Math.PI)));
+          const degrees = Math.round(Math.asin(parseFloat(sin)) * (180 / Math.PI));
+          setPosition(degrees);
         }
         setRotateClockwise(!rotateClockwise);
       }}
