6c8a9bf7372178956cbf00b084cbe00fcb580fb3
Daniel Steman <daniel-steman@live.nl>
2022-10-14 18:13:26 +0200

    blog 4 WIP

---
+++ public/content/blog4.md
@@ -0,0 +1,17 @@
+# WASM with React and Rust :crab:
+WebAssembly (WASM) is a new approach towards web development, which leverages the speed and robustness of lower level languages such as C, C++ and Rust to power websites. Today I started following [this book](https://rustwasm.github.io/docs/book/game-of-life/setup.html) and felt like writing about my experience with WASM. `wasm-bindgen` is a crate (which is was packages in Rust are called) that is used to interface with Javascript and `wasm-pack` compiles Rust code to WASM.
+
+&nbsp;
+
+Build your Rust project, which incorporates `wasm-bindgen` as a dependency with `wasm-pack`:
+
+`wasm-pack build`
+
+&nbsp;
+
+The command above generates a `pkg/` with artifacts. `create-wasm-app` is a Javascript scaffolding tool that makes it easier to setup a website that incorporates WASM:
+
+`npm init wasm-app www`
+
+&nbsp;
+
--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -7,21 +7,27 @@ const Blog = () => {
   const blogs = [
     {
       id: 1,
-      title: 'Monitor your houseplants with a NodeMCU ESP8266 microcontroller and a capacitive sensor',
+      title: 'Monitor your houseplants with a NodeMCU ESP8266 microcontroller and a capacitive sensor 🌱',
       date: 'Mon 1 Aug, 2022',
       text: 'Often, it\'s hard to observe moisture in soil, making it hard to judge whether your houseplants need water. This is a problem that can be countered with sensors. Of course, sensors need to be hooked up to a device to interpret and process signals. I was looking for a lightweight Arduino-like piece of hardware with integrated Wi-Fi and found the ESP8266.',
     },
     {
       id: 2,
-      title: 'BYOB: Build Your Own Blog',
+      title: 'BYOB: Build Your Own Blog 🖊️',
       date: 'Thu 1 Sep, 2022',
       text: 'Maybe you\'re just like me and you\'re doing several side projects. Some of them you complete, some of them you put back on the shelf to maybe pick up at a later point in time, or never. Either way, it can be rewarding to look back from time to time and see what you\'ve been doing over the years.',
     },
     {
       id: 3,
-      title: 'First steps towards a home lab',
+      title: 'First steps towards a home lab ☁️',
       date: 'Wed 12 Oct, 2022',
       text: 'Recently I have bought an Intel NUC, primarily to self-host Home Assistant and some other software that you would expect in a home lab.',
+    },
+    {
+      id: 4,
+      title: 'WASM with React and Rust 🦀',
+      date: 'Fri 14 Oct, 2022',
+      text: 'Web Assembly (WASM) is a new approach towards web development, which leverages the speed and robustness of lower level languages such as C, C++ and Rust to power websites.',
     },
   ];
   return (
