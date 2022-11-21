7f47f9ccf0e1fe333474d5bd342d02230da90df7
Daniel Steman <daniel-steman@live.nl>
2022-11-21 21:54:40 +0100

    :construction: blog5 wip

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -35,7 +35,7 @@ const Blog = () => {
     },
     {
       id: 5,
-      title: 'ML model serving with Seldon Core (MLOps part 1) 🤖🧠',
+      title: 'ML model serving with Seldon Core 🤖🧠',
       date: 'TBD',
       text: 'In this series of posts I want to write about my journey towards a fully functional machine learning operations (MLOps) ecosystem that I started working on.',
       tags: ['Machine learning'],
