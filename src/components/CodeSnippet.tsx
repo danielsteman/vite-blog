f63161a4af8022295a763708af5e7bc225be99f1
Daniel Steman <daniel-steman@live.nl>
2022-12-09 14:03:48 +0100

    :lipstick: fix horizontal scroll in blog post on mobile

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Code } from '@chakra-ui/react';
 
 const CodeSnippet = (props: any) => (
-  <Code>
+  <Code
+    variant="solid"
+    height="fit-content"
+    maxW="100%"
+    overflow="auto"
+  >
     {props.children}
   </Code>
 );
