e5c5be17e228d482afaeba801e0ec77493d878d9
Daniel Steman <daniel-steman@live.nl>
2023-06-22 15:00:59 +0000

    :lipstick: change background colors

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,9 +1,23 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import {
+  extendTheme,
+  StyleFunctionProps,
+  type ThemeConfig,
+} from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const customTheme = extendTheme({ config });
+const customTheme = extendTheme({
+  config,
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        color: "default",
+        bg: props.colorMode === "dark" ? "222222" : "FAFAFA",
+      },
+    }),
+  },
+});
 
 export default customTheme;
